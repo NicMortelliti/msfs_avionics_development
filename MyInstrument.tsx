4f3d0d8341537b5296a2aa4a1aded2b675ffed25
Nic Mortelliti <npmortelliti@gmail.com>
2023-03-23 16:17:30 -0700

    and component render

--- MyInstrument.tsx
+++ MyInstrument.tsx
@@ -1,4 +1,6 @@
 /// <reference types="@microsoft/msfs-types/Pages/VCockpit/Core/VCockpit" />
+import { FSComponent } from "@microsoft/msfs-sdk";
+import { MyComponent } from "./MyComponent";
 
 class MyInstrument extends BaseInstrument {
   // Note that this class contains the property templateID,
@@ -7,6 +9,20 @@ class MyInstrument extends BaseInstrument {
   // template you provided in the HTML, in this case MyInstrument.
   get templateID(): string {
     return "MyInstrument";
+  }
+
+  public connectedCallback(): void {
+    //! Always call super.connectedCallback() first
+    //! thing in any implementation of BaseInstrument.connectedCallback().
+    //! The base class does important work that is necessary for your 
+    //! instrument to function inside its implementation.Failing to call
+    //! super will cause your instrument to fail during load.
+    super.connectedCallback();
+
+    FSComponent.render(
+      <MyComponent />,
+      document.getElementById("InstrumentContent")
+    );
   }
 }
 
