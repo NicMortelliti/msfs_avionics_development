c04b8a84c389a4bc3a84d5b34a31bb167867dc38
Nic Mortelliti <npmortelliti@gmail.com>
2023-03-28 13:35:38 -0700

    Update comment

--- MyComponent.tsx
+++ MyComponent.tsx
@@ -1,6 +1,8 @@
 import { FSComponent, DisplayComponent, VNode } from "@microsoft/msfs-sdk";
 
-//? What's going on with the <any> tag?
+// * <any> is used to indicate the `MyComponent` class extends
+// * the `DisplayComponent` class with a type parameter of `any`,
+// * which means that the class can receive any type of data as input.
 export class MyComponent extends DisplayComponent<any> {
   public render(): VNode {
     return <div class="my-component">Hello World!</div>;
